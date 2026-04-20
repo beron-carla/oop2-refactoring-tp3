fc622b190462a1ecda734a58dfc864106bf74210
beron-carla <archcarlab@outlook.com.ar>
2026-04-20 12:23:04 -0300

    codigo ejercicio 3

---
+++ src/main/java/ejercicio3/EjemploDeUso.java
@@ -0,0 +1,14 @@
+package ejercicio3;
+
+
+import java.util.List;
+
+public class EjemploDeUso {
+    public static void main(String[] args) {
+        var g1 = new Gasto();
+        g1.tipoGasto = TipoDeGasto.DESAYUNO;
+        g1.monto =1000;
+        var reporte = new ReporteDeGastos();
+        reporte.imprimir(List.of(g1));
+    }
+}
